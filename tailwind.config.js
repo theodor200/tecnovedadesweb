6b88e209c6ab3659b8201cd9a7e84e69563e1b93
Theodor Cárdenas <theodorcardenas@gmail.com>
2021-11-04 23:38:41 -0500

    Se agrego un breakpoint XS de 320px

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,20 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
+   
     extend: {
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif']
-      },
+      },     
       /*backgroundImage: {
         'background': "url('/public/img/background.svg')"
       }*/
